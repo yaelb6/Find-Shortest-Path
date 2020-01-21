d367616822777e0adb5851d308342b7bbf210da5
Jenny Davidov <you@example.com>
2020-01-21 15:50:28 +0200

    With Search Algoritms

--- Astar.h
+++ Astar.h
@@ -5,10 +5,18 @@
 #ifndef EX4_ASTAR_H
 #define EX4_ASTAR_H
 
+#include "Searcher.h"
+#include "Searcher.h"
 
-class Astar {
-
+class Astar : public Searcher{
+    template<typename T>
+    vector<Point> search(Searchable<T> algoritm);
 };
 
+
+template<typename T>
+vector<Point> Astar::search(Searchable<T> algoritm) {
+    return vector<Point>();
+}
 
 #endif //EX4_ASTAR_H
---
+++ Matrix.h
@@ -0,0 +1,17 @@
+//
+// Created by jenny on 21/01/2020.
+//
+
+#ifndef EX4_MATRIX_H
+#define EX4_MATRIX_H
+
+
+#include "Searchable.h"
+
+template<typename T>
+class Matrix : public Searchable<T> {
+
+};
+
+
+#endif //EX4_MATRIX_H
